f1fdf45cd27daedef07e9652df6799ecd487dede
puhrome <phirom.yim@fullsail.edu>
2014-05-28 11:16:08 -0400

    Conditional 4 Types
    Description of the 4 types of procedural code. Explained what a
    conditional is.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Phirom Yim on 5/20/14.
+
+ The 4 Types of Procedural Code
+
+ Sequential - expressions simple order step by step
+
+ Conditional - exceptions, step added or skipped over
+ Need the machine to make the decision if the condition fits,
+ if it doesn't it skips over it
+
+ */
